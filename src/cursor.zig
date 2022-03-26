53327d29d94c3340b22e72ffa68af843d0aa428c
leesongun <12179851+leesongun@users.noreply.github.com>
2022-03-26 22:21:07 +0900

    remove cursor

--- src/cursor.zig
+++ src/cursor.zig
@@ -1,62 +1,42 @@
-const std = @import("std");
-const os = std.os.linux;
+const write = @import("std").os.linux.write;
 
-pub const cursor = packed struct {
-    const Self = @This();
-    pub var printer = "\x1B[0;0H.".*;
-    y: u3,
-    x: u3,
-    dir: u2,
-    pub fn move(self: *Self) !void {
-        const add = std.math.add;
-        const sub = std.math.sub;
-        switch (self.dir) {
-            0 => self.y = try sub(u3, self.y, 1),
-            1 => self.x = try sub(u3, self.x, 1),
-            2 => self.y = try add(u3, self.y, 1),
-            3 => self.x = try add(u3, self.x, 1),
-        }
-    }
-    pub fn move2(self: *Self) void {
-        const a = @ptrCast(*u8, self);
-        switch (a.* >> 6) {
-            0 => a.* -= 1,
-            1 => a.* -= 8,
-            2 => a.* += 1,
-            3 => a.* += 8,
-            else => unreachable,
-        }
-    }
-    pub fn move3(self: *Self) ?void {
-        const b = @bitCast(u8, self.*);
-        var a = b;
-        switch (a >> 6) {
-            0 => a -= 1,
-            1 => a -= 8,
-            2 => a += 1,
-            3 => a += 8,
-            else => unreachable,
-        }
-        if (@popCount(u8, a ^ b) > 3) return null;
-        self.* = @bitCast(Self, a);
-    }
-    pub fn index(self: Self) u6 {
-        return @as(u6, self.x) * 8 + self.y;
-    }
-    pub fn mask(self: Self) u64 {
-        return @as(u64, 1) << self.index();
-    }
-    pub fn print(self: Self, char: u8) void {
-        mcursor(self.x, self.y, char);
-    }
-    pub fn mcursor(x: u8, y: u8, char: u8) void {
-        printer["\x1B[".len] = '2' + x;
-        printer["\x1B[0;".len] = '2' + y;
-        printer[printer.len - 1] = char;
-        _ = os.write(1, &printer, printer.len);
-    }
-};
-
-test {
-    try std.testing.expectEqual(@bitCast(u8, cursor{ .y = 7, .x = 4, .dir = 2 }), 0o247);
+pub var printer = "\x1B[0;0H.".*;
+pub fn move(c: u8) u8 {
+    return switch (c >> 6) {
+        0 => c - 1,
+        1 => c - 8,
+        2 => c + 1,
+        3 => c + 8,
+        else => unreachable,
+    };
+}
+pub fn check_move(c: u8) ?u8 {
+    const r = move(c);
+    if (@popCount(u8, r ^ c) > 3) return null;
+    return r;
+}
+pub fn index(c: u8) u6 {
+    return @truncate(u6, c);
+}
+pub fn mask(c: u8) u64 {
+    return @as(u64, 1) << @truncate(u6, c);
+}
+pub fn getdir(c: u8) u2 {
+    return @truncate(u2, c >> 6);
+}
+pub fn xordir(c: *u8, d: u2) void {
+    c.* ^= @as(u8, d) << 6;
+}
+pub fn setdir(c: *u8, d: u2) void {
+    c.* &= 0o77;
+    c.* |= @as(u8, d) << 6;
+}
+pub fn print(c: u8, char: u8) void {
+    printer["\x1B[".len] = @as(u8, '2') + @truncate(u3, c >> 3);
+    printer["\x1B[0;".len] = @as(u8, '2') + @truncate(u3, c);
+    printer[printer.len - 1] = char;
+    _ = write(1, &printer, printer.len);
+}
+pub fn init(x: u3, y: u3, dir: u2) u8 {
+    return (@as(u8, dir) << 6) + (@as(u8, x) << 3) + y;
 }
--- src/snake.zig
+++ src/snake.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const os = std.os.linux;
-const cursor = @import("cursor.zig").cursor;
+const cursor = @import("cursor.zig");
+const getdir = cursor.getdir;
+const xordir = cursor.xordir;
+const index = cursor.index;
 
 //pub const init = ("." ** 8 ++ "\n") ** 7 ++ "." ** 8;
 pub const init = ("l" ++ "q" ** 8 ++ "k\n") ++ ("x\t x\n") ** 8 ++ ("m" ++ "q" ** 8 ++ "j");
@@ -8,83 +11,83 @@ pub const init = ("l" ++ "q" ** 8 ++ "k\n") ++ ("x\t x\n") ** 8 ++ ("m" ++ "q" *
 fn blank() u64 {
     return ~map[0] & ~map[1];
 }
-fn zero(index: u6) void {
-    map[0] &= ~(@as(u64, 1) << index);
-    map[1] &= ~(@as(u64, 1) << index);
+fn zero(ind: u6) void {
+    map[0] &= ~(@as(u64, 1) << ind);
+    map[1] &= ~(@as(u64, 1) << ind);
 }
-fn set(index: u6, value: u2) void {
-    map[0] |= @as(u64, value >> 1) << index;
-    map[1] |= @as(u64, value & 1) << index;
+fn set(ind: u6, value: u2) void {
+    map[0] |= @as(u64, value >> 1) << ind;
+    map[1] |= @as(u64, value & 1) << ind;
 }
-fn get(index: u6) u2 {
-    return (@truncate(u2, map[0] >> index) << 1) |
-        @truncate(u1, map[1] >> index);
+fn get(ind: u6) u2 {
+    return (@truncate(u2, map[0] >> ind) << 1) |
+        @truncate(u1, map[1] >> ind);
 }
 fn newfruit() ?void {
-    const bb = blank() ^ head.mask();
+    const bb = blank() ^ cursor.mask(head);
     if (bb == 0) return null;
     const r = rand.random()
         .uintLessThanBiased(u6, @intCast(u6, @popCount(u64, bb)));
     fruit = @intCast(u6, @ctz(u64, @import("utils.zig").getbit(bb, r)));
-    cursor.mcursor(@truncate(u3, fruit >> 3), @truncate(u3, fruit), '*');
+    cursor.print(fruit, '*');
 }
 
 //change blank to 2
 var map = [2]u64{ 0, 0 };
 var fruit: u6 = undefined;
-var head: cursor = .{ .dir = 2, .x = 4, .y = 0 }; //change this later
-var tail: cursor = .{ .dir = 2, .x = 4, .y = 0 };
+var head = cursor.init(4, 0, 2);
+var tail = cursor.init(4, 0, 2);
 //should actually read /dev/urandom 8bytes
 var rand = std.rand.Sfc64.init(1);
 pub fn main() void {
     newfruit() orelse unreachable;
 
     while (true) {
-        head.print("<^>V"[head.dir]);
+        cursor.print(head, "<^>V"[getdir(head)]);
 
         const temp = os.timespec{ .tv_sec = 0, .tv_nsec = 1_5000_0000 };
         _ = os.nanosleep(&temp, null);
 
-        const newdir = scandir();
+        const newdir = scandir(getdir(head));
         printneck(newdir);
 
-        set(head.index(), head.dir ^ newdir);
-        head.dir = newdir ^ 2;
-        head.move3() orelse break;
+        set(index(head), getdir(head) ^ newdir);
+        cursor.setdir(&head, newdir ^ 2);
+        head = cursor.check_move(head) orelse break;
 
-        if (head.index() != fruit) {
-            tail.dir ^= 2 ^ get(tail.index());
-            zero(tail.index());
-            if (head.mask() & blank() == 0) //die
+        if (index(head) != fruit) {
+            xordir(&tail, 2 ^ get(index(tail)));
+            zero(index(tail));
+            if (cursor.mask(head) & blank() == 0) //die
                 break;
-            //if (tail.index() != head.index())
-            tail.print(' ');
+            //if (tail.index() != index(head))
+            cursor.print(tail, ' ');
             //tail.move() catch unreachable;
-            tail.move2();
+            tail = cursor.move(tail);
         } else newfruit() orelse break;
     }
 }
 
-fn scandir() u2 {
+fn scandir(curdir: u2) u2 {
     while (true) {
         var buff: [1]u8 = undefined;
         if (os.read(0, &buff, 1) == 0)
-            return head.dir ^ 2;
-        var newnewdir: u2 = switch (buff[0]) {
+            return curdir ^ 2;
+        var newdir: u2 = switch (buff[0]) {
             'D' => 2, // 'D', 'H', 'h', 'a' => 2,
             'C' => 0, // 'C', 'L', 'l', 'd' => 0,
             'B' => 1, // 'B', 'J', 'j', 's' => 1,
             'A' => 3, // 'A', 'K', 'k', 'w' => 3,
             else => continue,
         };
-        if (newnewdir != head.dir)
-            return newnewdir;
+        if (newdir != curdir)
+            return newdir;
     }
 }
 
 fn printneck(newdir: u2) void {
     const arrows = "xjxk" ++ "qlqm";
-    const suffix = (head.dir == 0) or (newdir == 0);
+    const suffix = (getdir(head) == 0) or (newdir == 0);
     const t = @as(u3, @boolToInt(suffix)) << 2;
-    head.print(arrows[(head.dir ^ newdir) + t]);
+    cursor.print(head, arrows[(getdir(head) ^ newdir) + t]);
 }
