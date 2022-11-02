5fc8e241d1c8c86c0eebc95ee84f28de8ab07297
leesongun <12179851+leesongun@users.noreply.github.com>
2022-11-02 20:17:44 +0900

    vectorize

--- src/map.zig
+++ src/map.zig
@@ -1,19 +1,18 @@
 const Self = @This();
 
 pub fn blank(self: Self) u64 {
-    return ~self._[0] & ~self._[1];
+    return ~@reduce(.Or, self._);
 }
 pub fn zero(self: *Self, ind: u6) void {
-    self._[0] &= ~(@as(u64, 1) << ind);
-    self._[1] &= ~(@as(u64, 1) << ind);
+    self._ &= @splat(2, ~(@as(u64, 1) << ind));
 }
 pub fn set(self: *Self, ind: u6, value: u2) void {
-    self._[0] |= @as(u64, value >> 1) << ind;
-    self._[1] |= @as(u64, value & 1) << ind;
+    const _m = @bitCast(@Vector(2, u1), value);
+    self._ |= @as(@Vector(2, u64), _m) << @splat(2, ind);
 }
 pub fn get(self: Self, ind: u6) u2 {
-    return (@truncate(u2, self._[0] >> ind) << 1) |
-        @truncate(u1, self._[1] >> ind);
+    const x = (self._ >> @splat(2, ind) & @splat(2, @as(u64, 1))) << .{ 0, 1 };
+    return @intCast(u2, @reduce(.Or, x));
 }
 
 //change blank to 2
